5349ce0aae1b1274d805a5f7ba7fbf827e8d7308
zafergunayonalan <zafergunay7@gmail.com>
2023-06-12 00:39:00 +0300

    Degiskenler

---
+++ Ders1/ders1.js
@@ -0,0 +1,22 @@
+//console.log("Merhaba Dünya");
+//*CONSOLE.LOG ekrana veri bastıkrmak için kullanılır.
+
+//!DEĞİŞKENLER
+//?değişkenler bizler için veri tutar
+
+// let isim="zafer";
+// console.log(isim);
+
+// let isim = "Zafer";
+// let soyİsim = "Önalan";
+// console.log(isim+soyİsim);
+
+//!DEĞİŞKEN OLUŞTURMA KURALLARI
+//*değişkenler baş harfleri sayısal ifadeler ile başlamaz!
+// let 1isim = "zafer"
+
+//*değişkenler arasına boşluk bırakılmaz!
+// let isim soyisim = "zafer önalan"
+
+//*değişkenler arasına özel karakter koyulmaz!
+// let isim!^+%&/() = "zafer"
