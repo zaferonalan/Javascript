f5176dec934827426245877a9cad3bb7285a656b
zafergunayonalan <zafergunay7@gmail.com>
2023-06-13 22:58:43 +0300

    DısarıdanVeriGirisi

---
+++ Ders3/ders3.js
@@ -0,0 +1,41 @@
+// let sayi1 = 10;
+// let sayi2 = 5;
+
+// let toplam = sayi1 +sayi2;
+// toplam++;
+// alert(toplam);
+
+
+// let ad = "zafer günay";
+// let soyad = "Önalan";
+// let yas = 25;
+
+// console.log("Merhaba benim adım" + " " + ad + " soyadım" + " "+ soyad + " " + yas + " yaşındayım")
+
+
+// let name = prompt("Ad-Soyad giriniz :");
+// let birtday = prompt("Dogum Tarihii Giriniz :");
+// console.log(name + birtday);
+
+
+// let number = prompt("Sayi giriniz :");
+// let result = number*3;
+// console.log(result);
+
+
+// let number = prompt("Sayi giriniz :");
+// let result = number*number;
+// console.log(result);
+
+
+// let number1 = +prompt("1.Sayi giriniz :");
+// let number2 = +prompt("2.Sayi giriniz :");
+// let number3 = +prompt("3.Sayi giriniz :");
+// let result = (number1+number2+number3)/3
+// console.log(result);
+
+
+// let number1 = +prompt("Üçgenin kenar uzunlugu :");
+// let number2 = +prompt("Üçgenin yüksekliği :");
+// let result = (number1*number2)/2;
+// console.log(result);
