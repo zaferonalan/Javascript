81959f8ebe3154b498a93a0869b67c4d7ae7472f
zafergunayonalan <zafergunay7@gmail.com>
2023-08-01 14:05:45 +0300

    Yorum yap

--- Yorum Yap/App.js
+++ Yorum Yap/App.js
@@ -3,16 +3,50 @@ document.querySelector(".yorumYap").addEventListener("click",function()
     let kadi = document.querySelector(".kadi").value
     let mesaj = document.querySelector(".mesaj").value
 
-    
+    if (kadi != "" && mesaj != "") 
+    {
+        document.querySelector(".alert-danger").style.display="none"
+        document.querySelector(".yorumDurum").style.display="none"
+
+        let yorumlar = JSON.parse(localStorage.getItem("yorumlar")) || [];
+        let atilanYorum={
+        kullaniciAdi:kadi,
+        kullaniciYorum:mesaj
+        }
+        yorumlar.push(atilanYorum)
+
+        localStorage.setItem("yorumlar", JSON.stringify(yorumlar))
+    }
+    else{
+        document.querySelector(".alert-danger").style.display="block"
+    }
+
+    YorumKontrol()
 })
 
-//sayfa hazı olduğunda
 document.addEventListener("DOMContentLoaded",function()
 {
-    let yorumlar = localStorage.getItem("yorumlar") || []
-    if (yorumlar.length==0) 
+    YorumKontrol();
+})
+
+function YorumKontrol() 
+{
+    let yorumlar = JSON.parse(localStorage.getItem("yorumlar")) || [];
+    
+    if (yorumlar.length > 0) 
     {
-        console.log("yorumunuz bulunmamakta");    
+        let yorumYazdir = yorumlar.map((value) =>{
+            return`
+                <div class="yorumCard mt-4">
+                    <h5>${value.kullaniciAdi}</h5>
+                    <p>${value.kullaniciYorum}</p>
+                </div>
+            `
+        })
+        document.querySelector(".yorumlar").innerHTML=`${yorumYazdir.join("")}`
+    }
+    else{
+        document.querySelector(".yorumDurum").innerHTML="Henüz yorumunuz yok"
     }
     
-})
+}
